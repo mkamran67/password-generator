17f2f773f1635a0f23695dc4cfb8119164de8f70
Muhammad Kamran <mkamran1967@gmail.com>
2020-08-01 21:25:08 -0700

    Done

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // Document Listeners
-let generateBtn = document.querySelector('#generate');
+let generateBtn = document.querySelector('#submitBtn');
 let outPutText = document.getElementById('outputPassword');
-let numLength = document.getElementById('numLength');
+let numLength = document.querySelector('#numlengthTest');
 let lowercaseSelect = document.getElementById('lowercase');
 let uppercaseSelect = document.getElementById('uppercase');
 let numericSelect = document.getElementById('numeric');
 let specialSelect = document.getElementById('specialCharacters');
+
+// Start -- Main button
 generateBtn.addEventListener('click', writePassword);
 
 // Creates a password.
-function gemeratePassword() {
+function generatePassword() {
   let lowerCaseStr = 'abcdefghijklmnopqrstuvwxyz';
   let upperCaseStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let numericStr = '1234567890';
@@ -19,8 +21,8 @@ function gemeratePassword() {
 
   // Function takes in
   // 1. length 8 -- 128MAX? Validation If NOT VALID alert and ignore.
-  if (!numLength.checkValidity()) {
-    console.alert(`Invalid input please input a number between 8 - 128.`);
+  if (!numLength.checkValidity() || numLength === null) {
+    window.alert(`Invalid input please input a number between 8 - 128.`);
   } else {
     // Since it's a valid numeric entry continue...
 
@@ -43,11 +45,11 @@ function gemeratePassword() {
   }
   // Validate -> At least 1 type should be selected
   if (testStr === '') {
-    console.alert(`You must select at least one of the checkboxes`);
+    window.alert(`You must select at least one of the checkboxes`);
   } else {
     for (let i = 1; i < numLength.value; i++) {
       // get a random number (index) based on str length
-      let char = Math.floor(Math.random() * str.length + 1);
+      let char = Math.floor(Math.random() * testStr.length + 1);
 
       // Push that random index chracter into genPass
       genPass += testStr.charAt(char);
@@ -67,15 +69,25 @@ function onClickCopy() {
   textToCopy.select();
   textToCopy.setSelectionRange(0, 300);
   document.execCommand('copy');
-  alert(`Copied the password to your clipboard: ${textToCopy}`);
+  window.alert(`Copied the password to your clipboard: ${textToCopy.value}`);
 
   return;
 }
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(e) {
+  e.preventDefault();
+
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  // if return empty aka failed
+  if (password === '') {
+    password = 'Invalid Entries Try Again.';
+  }
+  var passwordText = document.querySelector('#outputPassword');
 
   passwordText.value = password;
 }
+
+document
+  .getElementById('outputPassword')
+  .addEventListener('click', onClickCopy);
